28912da43ba1abcebf9ac812abe5cbc935f1302d
CWillbanks <curtiswillbanks@gmail.com>
2015-11-19 15:22:39 -0500

    Added JPA entity beans

---
+++ src/java/edu/ccsu/beans/Customer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ccsu.beans;
+
+import java.beans.*;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Curtis
+ */
+@Entity
+public class Customer implements Serializable {
+    @Id
+    @GeneratedValue
+    private String email;
+    private String username;
+    
+    public Customer() {
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String newUsername) {
+        username = newUsername;
+    }
+    
+}
