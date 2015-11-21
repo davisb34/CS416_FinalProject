47fe493a86272680455406c0fedaa4f75e8761a8
Lauren Signor <Lauren.Signor@gmail.com>
2015-11-20 21:08:30 -0500

    deleted unneeded servlets and and added AJAX to addImage

--- src/java/edu/ccsu/beans/Image.java
+++ src/java/edu/ccsu/beans/Image.java
@@ -7,12 +7,7 @@ package edu.ccsu.beans;
 
 import java.io.IOException;
 import java.io.Serializable;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.component.UIComponent;
-import javax.faces.component.html.HtmlInputText;
-import javax.faces.context.FacesContext;
-import javax.faces.validator.ValidatorException;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
