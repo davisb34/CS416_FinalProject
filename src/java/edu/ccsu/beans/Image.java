c4375d73bd286283d93df584b3ef1e0104d275bc
Bryan <davis34b@yahoo.com>
2015-12-02 23:49:40 -0500

    Update: database access works, data table displays list image titles

--- src/java/edu/ccsu/beans/Image.java
+++ src/java/edu/ccsu/beans/Image.java
@@ -7,21 +7,25 @@ package edu.ccsu.beans;
 
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Table;
 
 /**
  *
  * @author Curtis
  */
+@ManagedBean
 @Entity
+//@ManagedProperty (value = "#{image}")
+//@Table (name = "image")
 public class Image implements Serializable {
     @Id
     @GeneratedValue
     private String title;
-    
     @Lob
     private byte[] content;
     
--- src/java/edu/ccsu/controller/ImageController.java
+++ src/java/edu/ccsu/controller/ImageController.java
@@ -8,14 +8,15 @@ package edu.ccsu.controller;
 import edu.ccsu.beans.Image;
 import java.io.ByteArrayOutputStream;
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.transaction.UserTransaction;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Query;
 import javax.servlet.http.Part;
@@ -32,7 +33,10 @@ public class ImageController {
     @Resource
     private UserTransaction userTransaction;
     private Part art;
+  //  
+    
     private String title;
+    private Image image;
     
     
     public String saveImage() throws IOException {
@@ -41,7 +45,7 @@ public class ImageController {
         try {
             userTransaction.begin();
             EntityManager entityManager = entityManagerFactory.createEntityManager();
-            Image image = new Image();
+            image = new Image();
             InputStream input = art.getInputStream();
             ByteArrayOutputStream output = new ByteArrayOutputStream();
             byte[] buffer = new byte[10240];
@@ -59,18 +63,21 @@ public class ImageController {
         return returnVal;
     }
     
-    public List getAllImages() {
+   public List getretrieveImage() {
         List<Image> images = new ArrayList();
+        String result = "error";
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        String selectSQL = "select * from image";
+        String selectSQL = "select i from Image";
         try {
             Query selectQuery = entityManager.createQuery(selectSQL);
             images = selectQuery.getResultList();
+           // result = "Gallery";
         } catch (Exception e) {
             e.printStackTrace();
         }
         return images;
     }
+    
     
     public String getTitle() {
         return title;
