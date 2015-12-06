4faa5dded84ab179649a708bc2818888202f26f9
advancedclockwork <advancedclockwork@gmail.com>
2015-12-05 19:55:40 -0500

    pages look ok. let me know what kind of layout you want for the main page
    pages are up except contact. gallery page has a single pic, but try
    clicking on it. make sure that is what you want for the rest of the
    gallery

---
+++ build/web/js/Fancybox.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function() {
+
+	/* This is basic - uses default settings */
+	
+	$("a#single_image").fancybox();
+	
+	/* Using custom settings */
+	
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
+	});
+
+	/* Apply fancybox to multiple items */
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
+});
